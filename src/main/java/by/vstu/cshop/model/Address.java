6cf3cb8b85acd129cb3a7a951ab7e75d28915076
Pavel Stankevich <pavel.stankevich@lacit.net>
2018-06-03 00:42:59 +0300

    first day_c2

--- src/main/java/by/vstu/cshop/model/Address.java
+++ src/main/java/by/vstu/cshop/model/Address.java
@@ -2,7 +2,10 @@ package by.vstu.cshop.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,5 +15,24 @@ import javax.persistence.Table;
 public class Address {
 
     @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
+    private String fio;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String region;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String postcode;
 }
--- src/main/java/by/vstu/cshop/model/Person.java
+++ src/main/java/by/vstu/cshop/model/Person.java
@@ -2,7 +2,10 @@ package by.vstu.cshop.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
@@ -15,12 +18,24 @@ import java.util.List;
 public class Person {
 
     @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String surname;
+
+    @Column(nullable = false)
     private String patronymic;
+
+    @Column(nullable = false)
     private Date birthday;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
     @ManyToMany
     private List<Address> addresses;
 }
